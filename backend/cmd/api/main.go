5b9447322cc36ade9ec3aa3c7c87baa0e1f204f7
kokocuk268 <mishazol@6rus@gmail.com>
2026-07-28 14:50:15 +0300

    feature(backend): добавил таймауты и корректное завершение сервера

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 type Resp struct {
@@ -15,18 +21,56 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	js := Resp{Status: "ok"}
 	jsonByte, err := json.Marshal(js)
 	if err != nil {
-		fmt.Fprintln(w, http.StatusInternalServerError)
+		http.Error(w, "ошибка 500", http.StatusInternalServerError)
 		return
 	}
 	fmt.Fprintln(w, string(jsonByte))
 }
 
 func main() {
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+	defer stop()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /health/live", healthHandler)
+	errCh := make(chan error, 1)
 
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	serv := http.Server{
+		Addr:              ":8080",
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
+
+	go func() {
+		if err := serv.ListenAndServe(); err != nil {
+			if errors.Is(err, http.ErrServerClosed) {
+				return
+			}
+
+			errCh <- err
+		}
+	}()
+	select {
+	case <-ctx.Done():
+		// пришёл ctrl + c или sigterm
+		fmt.Println("Получен сигнал завершения")
+	case err := <-errCh:
+		// cервер упал с ошибкой
 		fmt.Println(err)
 		return
 	}
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+	defer cancel()
+	if err := serv.Shutdown(shutdownCtx); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Сервер корректно остановлен!👌")
 }
