96ea25882f3eb89a8ced60716a08ec386d56be39
kokocuk268 <mishazol@6rus@gmail.com>
2026-08-08 15:54:13 +0300

    ci: добавил go vet и gofmt, отформатировал user.go

--- backend/internal/domain/user.go
+++ backend/internal/domain/user.go
@@ -3,8 +3,8 @@ package domain
 import "time"
 
 type User struct {
-	ID string
-	Email string
+	ID           string
+	Email        string
 	PasswordHash string
-	CreatedAt time.Time
+	CreatedAt    time.Time
 }
